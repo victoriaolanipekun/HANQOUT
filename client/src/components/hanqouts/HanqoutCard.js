d38f1ce080a101921ed42750a2e213e5d75ee403
Victoria Olanipekun <queentoria1@gmail.com>
2021-07-30 10:39:46 +0100

    more styling for the cards

--- client/src/components/hanqouts/HanqoutCard.js
+++ client/src/components/hanqouts/HanqoutCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const HanqoutCard = ({ id, title, description, image, price }) => {
   return (
-    <div className="column is-one-quarter-desktop is-one-third-tablet">
+    <div className="show-page column is-one-quarter-desktop is-one-third-tablet">
       <Link to={`/hanqout/${id}/`}>
         <div className="card">
           <div className="card-image">
@@ -15,12 +15,12 @@ const HanqoutCard = ({ id, title, description, image, price }) => {
             <div className="card-header-title">{title}</div>
           </div>
           <div className="card-content">
-            <h5>Description: {description} </h5>
-            <h5>Price: {price} </h5>
+            <h5>{description} </h5>
+            <h5><strong>Price: </strong>{price} </h5>
           </div>
           <div className="card-content">
-            <div>
-              <Link to="/profile/" className="button is-outlined is-rounded">Join Now</Link>
+            <div className="join-btn">
+              <Link to="/profile/" className="button is-danger is-light is-rounded"><strong>Join Now</strong></Link>
             </div>
           </div>
         </div>
